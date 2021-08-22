a0354327cdd4ee4787598ff37cd7873b693109f4
yunfangli <hanszimmerme@gmail.com>
2021-08-23 03:01:37 +0800

    update merge sort

--- src/SortAlgos.c
+++ src/SortAlgos.c
@@ -18,3 +18,28 @@ void InsertSort(int *nums, int numsSize) {
         }
     }
 }
+
+void MergeSort(int *nums, int numsSize) {
+    if (numsSize <= 1) return;
+    int tmpNums[numsSize];
+    int *p1 = memcpy(tmpNums, nums, numsSize * sizeof(int));
+    int remainSize = numsSize - numsSize / 2;
+    int *p2 = tmpNums + numsSize / 2;
+    MergeSort(p1, numsSize / 2);
+    MergeSort(p2, remainSize);
+    int pos1 = 0;
+    int pos2 = 0;
+    for (int i = 0; i < numsSize; i++) {
+        int tmp;
+        if (pos1 == numsSize / 2) {
+            tmp = p2[pos2++];
+        } else if (pos2 == remainSize) {
+            tmp = p1[pos1++];
+        } else if (p1[pos1] < p2[pos2]) {
+            tmp = p1[pos1++];
+        } else {
+            tmp = p2[pos2++];
+        }
+        nums[i] = tmp;
+    }
+}
--- src/SortAlgos.h
+++ src/SortAlgos.h
@@ -2,5 +2,6 @@
 #define SORT_ALGOS
 
 extern void InsertSort(int *nums, int numsSize);
+extern void MergeSort(int *nums, int numsSize);
 
 #endif
