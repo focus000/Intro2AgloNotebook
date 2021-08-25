add9aafbccddae71fb8736335160b84eefe624fa
yunfangli <hanszimmerme@gmail.com>
2021-08-26 00:46:45 +0800

    update test case

--- test/Test.cc
+++ test/Test.cc
@@ -1,12 +1,39 @@
 #include "gtest/gtest.h"
 #include "src/SortAlgos.h"
+#include <random>
+#include <vector>
+#include <algorithm>
 
 TEST(SortAlgos, InsertSort) {
-    int nums[] = {3, 23, 76, 98, 1, 98, 22, 4, 0, 8, -3};
-    int resNums[] = {-3, 0, 1, 3, 4, 8, 22, 23, 76, 98, 98};
-    int len = sizeof(nums) / sizeof(int);
-    InsertSort(nums, len);
+    std::random_device r;
+    std::default_random_engine el(r());
+    std::uniform_int_distribution<int> uniform_dist(INT32_MIN, INT32_MAX);
+    constexpr size_t len = 10000;
+    std::vector<int> v1, v2;
+    for (size_t i = 0; i < len; i++) {
+        v1.push_back(uniform_dist(el));
+    }
+    v2 = v1;
+    std::sort(v2.begin(), v2.end());
+    InsertSort(v1.data(), v1.size());
     for (int i = 0; i < len; i++) {
-        EXPECT_EQ(nums[i], resNums[i]) << " differ at index " << i;
+        EXPECT_EQ(v1, v2);
+    }
+}
+
+TEST(SortAlgos, MergeSort) {
+    std::random_device r;
+    std::default_random_engine el(r());
+    std::uniform_int_distribution<int> uniform_dist(INT32_MIN, INT32_MAX);
+    constexpr size_t len = 10000;
+    std::vector<int> v1, v2;
+    for (size_t i = 0; i < len; i++) {
+        v1.push_back(uniform_dist(el));
+    }
+    v2 = v1;
+    std::sort(v2.begin(), v2.end());
+    MergeSort(v1.data(), v1.size());
+    for (int i = 0; i < len; i++) {
+        EXPECT_EQ(v1, v2);
     }
 }
